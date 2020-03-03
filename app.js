c8ad1f5b69f677015691756366e81e684c4618da
asep19 <aseprendi487@gmail.com>
2020-03-03 11:31:11 +0700

    routes admin & halaman admin

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var expressHbs = require('express-handlebars');
 
+// routes
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var admin = require('./routes/admin');
 
 var app = express();
 
@@ -23,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/admin', admin);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
