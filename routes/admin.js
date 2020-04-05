8c09804a0481ff15ed57e241a2cf5fe8bfc91960
asep19 <aseprendi487@gmail.com>
2020-04-05 16:18:38 +0700

    upload foto berhasil

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var router = express.Router();
-const conn = require('../config/connection');
+const express = require('express');
+const router = express.Router();
+const conn = require('../config/connection'); 
 
 router.get('/', (req, res, next) => {
     res.render('admin/index')
@@ -13,49 +13,95 @@ router.route('/produk')
         let query = conn.query(sql, (err, results) => {
             if (err) throw err;
             res.render('admin/produk',{
-                results: results
+                results: results,
+                title: 'Dashboard Myhijab'
             });
         }) 
-});
-
-// router.get('/', (req, res, next) => {
-//     let sql = "SELECT * FROM produk";
-//     let query = conn.query(sql, (err, results) => {
-//         if (err) throw err;
-//         res.render('admin/index', {
-//             results: results
-//         });
-//     });
-// });
+}); 
 
 // tambah data
 router.post('/save', (req, res) => {
-    let data = { nama_produk: req.body.nama_produk, harga_produk: req.body.harga_produk };
-    let sql = "INSERT INTO produk SET ?";
-    let query = conn.query(sql, data, (err, results) => {
-        if (err) throw err;
-        res.redirect('/admin');
-        //res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
-    });
+    message = '';
+    if (req.method == "POST") {
+        let post = req.body;
+        let nama = post.nama_produk;
+        let harga = post.harga_produk;
+        let stok = post.stok;
+
+        if (!req.files)
+            return res.status(400).send('Tidak ada file yang diupload.');
+        
+        let file = req.files.foto_produk;
+        let img_name = file.name;
+
+        if (file.mimetype == "image/jpeg" || file.mimetype == "image/png" || file.mimetype == "image/gif") {
+            
+            file.mv('public/images/produk/' + file.name, function (err) {
+                if (err)
+                    return res.status(500).send(err);
+                // let data = {nama_produk: nama, harga_produk: harga, stok: stok, foto_produk: img_name};
+                // let sql = "INSERT INTO produk SET ?";
+                let sql = "INSERT INTO `produk`(`nama_produk`,`harga_produk`,`stok`,`foto_produk`) VALUES ('" + nama + "','" + harga + "','" + stok + "','" + img_name + "')";
+                let query = conn.query(sql, (err, results) => {
+                    if (err) throw err;
+                    res.redirect('/admin/produk');
+                });
+            
+            });
+
+        } else {
+            message = "Format ini tidak diizinkan, upload file dengan format '.png', '.jpeg', '.gif'";
+            res.render('index.hbs', {message: message});
+        }
+    } else {
+        res.render('index');
+    }
 });
+
+// tambah data
+// router.post('/save', (req, res) => {
+//     let data = {nama_produk: req.body.nama_produk, harga_produk: req.body.harga_produk, stok: req.body.stok };
+//     let sql = "INSERT INTO produk SET ?";
+//     let query = conn.query(sql, data, (err, results) => {
+//         if (err) throw err;
+//         res.redirect('/admin/produk');
+//     });
+    
+// });
+
+// tambah data
+// router.post('/save', (req, res) => {
+//     upload(req, res, err => {
+//         if (err) throw err;
+//         let data = { nama_produk: req.body.nama_produk, harga_produk: req.body.harga_produk, stok: req.body.stok, foto_produk: req.file.filename };
+//         let sql = "INSERT INTO produk SET ?";
+//         let query = conn.query(sql, data, (err, results) => {
+//             if (err) throw err;
+//             res.redirect('/admin/produk'); 
+//         });
+//     })
+
+// });
 
 // edit produk
 router.post('/update', (req, res) => {
-    let sql = "UPDATE product SET product_name ='" + req.body.product_name + "', product_price='" + req.body.product_price + "' WHERE product_id=" + req.body.id;
+    let sql = "UPDATE produk SET nama_produk ='" + req.body.nama_produk + "', harga_produk='" + req.body.harga_produk + "', stok='" + req.body.stok + "' WHERE id_produk=" + req.body.id;
     let query = conn.query(sql, (err, results) => {
         if (err) throw err;
         //res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
-        res.redirect('/');
+        res.redirect('/admin/produk');
     });
 });
 
 // delete produk
 router.post('/delete', (req, res) => {
-    let sql = "DELETE FROM produk WHERE id_produk=" + req.body.id_produk + "";
+    let sql = "DELETE FROM produk WHERE id_produk=" + req.body.id + "";
     let query = conn.query(sql, (err, results) => {
         if (err) throw err;
-        res.redirect('/admin');
+        res.redirect('/admin/produk');
     });
 });
+
+
 
 module.exports = router;
